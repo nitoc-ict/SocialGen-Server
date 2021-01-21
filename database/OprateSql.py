6435ff2354e426f6df71abbdf273166ac736c26a
k4r1nt <k4r1nt@gmail.com>
2021-01-22 08:53:40 +0900

    addUser() addResult()を追加、createTable()の使用を変更

--- database/OprateSql.py
+++ database/OprateSql.py
@@ -1,69 +1,98 @@
 import sqlite3
+import re
 
-dbname = 'PlayerRanking.db'
+dbname = 'scoreData.db'
 conn = sqlite3.connect(dbname)
 cur = conn.cursor()
 
 def createTable():
-    cur.execute('CREATE TABLE IF NOT EXISTS allJapan(date,id,name,score,pref)')
-    cur.execute('CREATE TABLE IF NOT EXISTS Hokkaido(date,id,name,score)')
-    cur.execute('CREATE TABLE IF NOT EXISTS Aomori(date,id,name,score)')
-    cur.execute('CREATE TABLE IF NOT EXISTS Iwate(date,id,name,score)')
-    cur.execute('CREATE TABLE IF NOT EXISTS Miyagi(date,id,name,score)')
-    cur.execute('CREATE TABLE IF NOT EXISTS Akita(date,id,name,score)')
-    cur.execute('CREATE TABLE IF NOT EXISTS Yamagata(date,id,name,score)')
-    cur.execute('CREATE TABLE IF NOT EXISTS Fukushima(date,id,name,score)')
-    cur.execute('CREATE TABLE IF NOT EXISTS Ibaraki(date,id,name,score)')
-    cur.execute('CREATE TABLE IF NOT EXISTS Tochigi(date,id,name,score)')
-    cur.execute('CREATE TABLE IF NOT EXISTS Gunma(date,id,name,score)')
-    cur.execute('CREATE TABLE IF NOT EXISTS Saitama(date,id,name,score)')
-    cur.execute('CREATE TABLE IF NOT EXISTS Chiba(date,id,name,score)')
-    cur.execute('CREATE TABLE IF NOT EXISTS Tokyo(date,id,name,score)')
-    cur.execute('CREATE TABLE IF NOT EXISTS Kanagawa(date,id,name,score)')
-    cur.execute('CREATE TABLE IF NOT EXISTS Niigata(date,id,name,score)')
-    cur.execute('CREATE TABLE IF NOT EXISTS Toyama(date,id,name,score)')
-    cur.execute('CREATE TABLE IF NOT EXISTS Ishikawa(date,id,name,score)')
-    cur.execute('CREATE TABLE IF NOT EXISTS Fukui(date,id,name,score)')
-    cur.execute('CREATE TABLE IF NOT EXISTS Yamanashi(date,id,name,score)')
-    cur.execute('CREATE TABLE IF NOT EXISTS Nagano(date,id,name,score)')
-    cur.execute('CREATE TABLE IF NOT EXISTS Gifu(date,id,name,score)')
-    cur.execute('CREATE TABLE IF NOT EXISTS Shizuoka(date,id,name,score)')
-    cur.execute('CREATE TABLE IF NOT EXISTS Aichi(date,id,name,score)')
-    cur.execute('CREATE TABLE IF NOT EXISTS Mie(date,id,name,score)')
-    cur.execute('CREATE TABLE IF NOT EXISTS Shiga(date,id,name,score)')
-    cur.execute('CREATE TABLE IF NOT EXISTS Kyoto(date,id,name,score)')
-    cur.execute('CREATE TABLE IF NOT EXISTS Osaka(date,id,name,score)')
-    cur.execute('CREATE TABLE IF NOT EXISTS Hyogo(date,id,name,score)')
-    cur.execute('CREATE TABLE IF NOT EXISTS Nara(date,id,name,score)')
-    cur.execute('CREATE TABLE IF NOT EXISTS Wakayama(date,id,name,score)')
-    cur.execute('CREATE TABLE IF NOT EXISTS Tottori(date,id,name,score)')
-    cur.execute('CREATE TABLE IF NOT EXISTS Shimane(date,id,name,score)')
-    cur.execute('CREATE TABLE IF NOT EXISTS Okayama(date,id,name,score)')
-    cur.execute('CREATE TABLE IF NOT EXISTS Hiroshima(date,id,name,score)')
-    cur.execute('CREATE TABLE IF NOT EXISTS Yamaguchi(date,id,name,score)')
-    cur.execute('CREATE TABLE IF NOT EXISTS Tokushima(date,id,name,score)')
-    cur.execute('CREATE TABLE IF NOT EXISTS Kagawa(date,id,name,score)')
-    cur.execute('CREATE TABLE IF NOT EXISTS Ehime(date,id,name,score)')
-    cur.execute('CREATE TABLE IF NOT EXISTS Kochi(date,id,name,score)')
-    cur.execute('CREATE TABLE IF NOT EXISTS Fukuoka(date,id,name,score)')
-    cur.execute('CREATE TABLE IF NOT EXISTS Saga(date,id,name,score)')
-    cur.execute('CREATE TABLE IF NOT EXISTS Nagasaki(date,id,name,score)')
-    cur.execute('CREATE TABLE IF NOT EXISTS Kumamoto(date,id,name,score)')
-    cur.execute('CREATE TABLE IF NOT EXISTS Oita(date,id,name,score)')
-    cur.execute('CREATE TABLE IF NOT EXISTS Miyazaki(date,id,name,score)')
-    cur.execute('CREATE TABLE IF NOT EXISTS Kagoshima(date,id,name,score)')
-    cur.execute('CREATE TABLE IF NOT EXISTS Okinawa(date,id,name,score)')
+    cur.execute("CREATE TABLE IF NOT EXISTS player(id, name, maxScore, area)")
+    cur.execute("CREATE TABLE IF NOT EXISTS totalScore(area, totalScore)")
+    '''
+    cur.executescript("""
+        INSERT INTO totalScore VALUES (allJapan,0)
+        INSERT INTO totalScore VALUES (Hokkaido,0)
+        INSERT INTO totalScore VALUES (Aomori,0)
+        INSERT INTO totalScore VALUES (Iwate,0)
+        INSERT INTO totalScore VALUES (Miyagi,0)
+        INSERT INTO totalScore VALUES (Akita,0)
+        INSERT INTO totalScore VALUES (Yamagata,0)
+        INSERT INTO totalScore VALUES (Fukushima,0)
+        INSERT INTO totalScore VALUES (Ibaraki,0)
+        INSERT INTO totalScore VALUES (Tochigi,0)
+        INSERT INTO totalScore VALUES (Gumma,0)
+        INSERT INTO totalScore VALUES (Saitama,0)
+        INSERT INTO totalScore VALUES (Chiba,0)
+        INSERT INTO totalScore VALUES (Tokyo,0)
+        INSERT INTO totalScore VALUES (Kanagawa,0)
+        INSERT INTO totalScore VALUES (Nigata,0)
+        INSERT INTO totalScore VALUES (Toyama,0)
+        INSERT INTO totalScore VALUES (Ishikawa,0)
+        INSERT INTO totalScore VALUES (Fukui,0)
+        INSERT INTO totalScore VALUES (Yamanashi,0)
+        INSERT INTO totalScore VALUES (Nagano,0)
+        INSERT INTO totalScore VALUES (Gifu,0)
+        INSERT INTO totalScore VALUES (Shizuoka,0)
+        INSERT INTO totalScore VALUES (Aichi,0)
+        INSERT INTO totalScore VALUES (Mie,0)
+        INSERT INTO totalScore VALUES (Shiga,0)
+        INSERT INTO totalScore VALUES (Kyoto,0)
+        INSERT INTO totalScore VALUES (Osaka,0)
+        INSERT INTO totalScore VALUES (Hyogo,0)
+        INSERT INTO totalScore VALUES (Nara,0)
+        INSERT INTO totalScore VALUES (Wakayama,0)
+        INSERT INTO totalScore VALUES (Tottori,0)
+        INSERT INTO totalScore VALUES (Shimane,0)
+        INSERT INTO totalScore VALUES (Okayama,0)
+        INSERT INTO totalScore VALUES (Hiroshima,0)
+        INSERT INTO totalScore VALUES (Yamaguchi,0)
+        INSERT INTO totalScore VALUES (Tokushima,0)
+        INSERT INTO totalScore VALUES (Kagawa,0)
+        INSERT INTO totalScore VALUES (Ehime,0)
+        INSERT INTO totalScore VALUES (Kochi,0)
+        INSERT INTO totalScore VALUES (Fukuoka,0)
+        INSERT INTO totalScore VALUES (Saga,0)
+        INSERT INTO totalScore VALUES (Nagasaki,0)
+        INSERT INTO totalScore VALUES (Kumamoto,0)
+        INSERT INTO totalScore VALUES (Oita,0)
+        INSERT INTO totalScore VALUES (Miyazaki,0)
+        INSERT INTO totalScore VALUES (Kagoshima,0)
+        INSERT INTO totalScore VALUES (Okinawa,0)
+    """)
+    '''
 
-def insertData(playerId :int, name :str, score :int,pref :str):
-    cur.execute("INSERT INTO allJapan VALUES (datetime('now', '+9 hours'),?,?,?,?)", (playerId, name, score, pref))
-    cur.execute("INSERT INTO " + pref + " VALUES (datetime('now', '+9 hours'),?,?,?)",(playerId, name, score))
+
+def addUser(id: int, name: str, area: str):
+    cur.execute("INSERT INTO player VALUES (?,?,?,?)", (id, name, 0, area)) # id重複時の処理はどうする
     conn.commit()
+    # check =
+    # return(check)
 
-def showRank():
-    let = cur.execute("SELECT * FROM allJapan ORDER BY score DESC OFFSET 0 ROWS FETCH NEXT 50 ROWS ONLY")
+def addResult(id: int, score: int):
+    cur.execute("")
+    cur.execute("SELECT maxScore FROM player WHERE id = ?", (id,))
+    hoge = cur.fetchone()
+    if hoge[0] < score:
+        cur.execute("UPDATE player SET maxScore = ? WHERE id = ?", (score, id))
 
+    conn.commit()
+    # check =
+    # return(check)
 
+''' Un Completed
+def getRank(id: int):
+    return(json,rank,score)
+
+def getTotalScore(area: str):
+    totalJP
+    totalarea
+    totalScore[] = {totalJP,totalarea}
+    return(totalScore[])
+'''
+
+''' test
 createTable()
-insertData(335, 'hundo', 50000, 'Okinawa')
-
+addUser(1, 'hundo', 'Okinawa')
+addResult(1, 60000)
+'''
 conn.close()
